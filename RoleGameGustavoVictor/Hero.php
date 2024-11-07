c71450030b5e4ea2e2c1daaaab766fd3864120b5
gusvicferser <gusvfs10@gmail.com>
2024-11-07 17:14:49 +0100

    heroQuest1Clase_07_11

--- RoleGameGustavoVictor/Hero.php
+++ RoleGameGustavoVictor/Hero.php
@@ -18,6 +18,9 @@ class Hero
     private int $health;
     private int $baseAttack;
     private int $baseDefense;
+    private array $weapons;
+    private Armor $armor;
+    private array $potions;
 
     function __construct(
         $name,
@@ -27,33 +30,41 @@ class Hero
         $baseAttack = 10,
         $baseDefense = 10
     ) {
+
         $this->name = $name;
-        
+
+        /* Hemos de chequear que la especie esté entre las opciones disponibles:
+         y si no se encuentra entre ellas, será 'Humano' la clase por defecto:*/
         if ($this->checkSpecies($species)) {
             $this->species = $species;
         } else {
             $this->species = 'Humano';
         }
 
+        /* También hemos de verificar que la clase sea la correcta y si no, 
+         ponemos que es ninguna:*/
         if ($this->checkClass($class)) {
             $this->class = $class;
         } else {
             $this->class = 'Ninguna';
         }
 
-        if ($health > 0){
+        /* La salud no será ni un número negativo ni cero:*/
+        if ($health > 0) {
             $this->health = $health;
         } else {
             $this->health = 1;
         }
 
-        if ($baseAttack > 0){
+        /* El ataque base no puede ser negativo (al crear el objeto héroe): */
+        if ($baseAttack > 0) {
             $this->baseAtack = $baseAttack;
         } else {
             $this->baseAtack = 0;
         }
 
-        if ($baseDefense > 0){
+        /* La defensa tampoco (al crear el héroe):*/
+        if ($baseDefense > 0) {
             $this->baseDefense = $baseDefense;
         } else {
             $this->baseDefense = 0;
@@ -72,11 +83,6 @@ class Hero
         if (isset($property) && is_int($value)) {
             $this->$property = $value;
         }
-    }
-
-    function __toString()
-    {
-        return $this->name . ': ' . $this->species . ', ' . $this->class . ', ' . $this->health . ' HP';
     }
 
     /**
@@ -137,5 +143,175 @@ class Hero
             ];
 
         return in_array($class, $classes);
+    }
+
+
+
+    /**
+     * Función pública para añadir armas al héroe siempre que no lleve ya dos
+     * (Que es el máximo permitido):
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @param: Weapon $weapon
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function addWeapon(Weapon $weapon): bool
+    {
+        if (count($this->weapons) < 2) {
+            array_push($this->weapons, $weapon);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Función pública para eliminar armas (que existan y que lleve el héroe) 
+     * al héroe en cuestión:
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @param: Weapon $weapon
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function rmvWeapon(Weapon $weapon): bool
+    {
+
+        if (in_array($weapon, $this->weapons)) {
+            $key = array_search($weapon, $this->weapons);
+            if (is_int($key)) {
+                $this->weapons = array_splice($this->weapons, $key, $key);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Función pública para atacar que suma el daño base del héroe con el int de
+     * ataque de todas sus armas:
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @return: int (el daño total)
+     */
+    function attack(): int
+    {
+
+        $dmg = $this->baseAttack;
+
+        if (count($this->weapons) > 0) {
+            foreach ($this->weapons as $weapon) {
+                $dmg += $weapon->get('attack');
+            }
+        }
+
+        return $dmg;
+    }
+
+    /**
+     * Función pública para defenderse que suma las defensas y luego le resta el
+     * daño hecho:
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @param:int (daño realizado)
+     * @return: int (el daño recibido)
+     */
+    function defense(int $dmg): int
+    {
+
+        
+
+        if (count($this->weapons) > 0) {
+            foreach ($this->weapons as $weapon) {
+                $dmg += $weapon->get('attack');
+            }
+        }
+
+        return $dmg;
+    }
+
+
+    /**
+     * Función pública para añadir armadura al héroe siempre que no lleve ya una
+     * (Que es el máximo permitido):
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @param: Armor $armor
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function addArmor(Armor $armor): bool
+    {
+        if ($this->armor != null) {
+            $this->armor = $armor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Función pública para eliminar la armadura al héroe en cuestión:
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function rmvArmor(): bool
+    {
+
+        if ($this->armor != null) {
+            $this->armor = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Función pública para añadir pociones al héroe siempre que no lleve ya
+     * tres (Que es el máximo permitido):
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @param: Potion $potion
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function addPotion(Potion $potion): bool
+    {
+        if (count($this->potions) < 3) {
+            array_push($this->potions, $potion);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Función pública para usar pociones (si el héroe tiene alguna). En caso de
+     * tener varias, usará la que más HP cure:
+     * 
+     * @author: Gustavo Víctor
+     * @version: 1.0
+     * @return: bool (true si lo ha hecho, false si no)
+     */
+    function usePotion()
+    {
+        if (count($this->potions) > 0) {
+            sort($this->potions);
+            $usedPotion = array_pop($this->potions);
+            $this->health += $usedPotion->get('health');
+            unset($usedPotion);
+        }
+    }
+
+    function __toString()
+    {
+        return $this->name . ': ' . $this->species . ', '
+            . $this->class . ', ' . $this->health . ' HP';
     }
 }

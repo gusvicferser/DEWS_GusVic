2800269a29682355e47ed1033a8df827790837b7
gusvicferser <gusferser@alu.edu.gva.es>
2024-11-17 02:11:24 +0100

    discografiaPrueba_17_11

--- DiscografiaGustavoVictor/index.php
+++ DiscografiaGustavoVictor/index.php
@@ -32,27 +32,59 @@ function connectDB(): mixed
 	}
 }
 
+// Si el formulario está vacío, mostramos todos los grupos con sus covers:
 if (empty($_GET)) {
 
+	// Establecemos la conexión con la función que hemos creado.
+	$connection = connectDB();
+
+	// En el caso de que la conexión no sea nula, obtenemos los grupos.
+	if ($connection != null) {
+		$results = $connection->query(
+			'SELECT id, name, photo FROM groups ORDER BY name ASC;'
+		); // La consulta está entregada por el ejercicio.
+
+		/* Obtenemos todos los resultados como objetos y los guardamos en un
+		 array de objetos:*/
+		$groups = $results->fetchAll(PDO::FETCH_OBJ);
+
+		// Deshacemos las variables para cortar la conexión a la base de datos:
+		unset($results);
+		unset($connection);
+	}
+} else {
 
 	$connection = connectDB();
 
 	if ($connection != null) {
-		$results = $connection->query(
-			'SELECT id, name, photo FROM groups ORDER BY name ASC;'
+		// Como en este caso el usuario introduce datos, hemos de asegurarnos de
+		// que su consulta no sea un injecto de sql:
+		$results = $connection->prepare(
+			'SELECT id,name,photo 
+			FROM groups 
+			WHERE name LIKE :search 
+			ORDER BY name ASC;'
 		);
 
-		foreach($results->fetchAll(PDO::FETCH_OBJ) as $result) {
-			
-		}
+		// bindParam() no admite comodines, por lo que creamos una variable donde
+		// los introducimos:
+		$key = '%'. trim($_GET['search']) . '%';
+
+		// Y ahora sí, ligamos los parámetros a la consulta:
+		$results->bindParam(':search', $key);
+
+		// Y ahora sí, ejecutamos la consulta:
+		$results->execute();
+
+		// Almacenamos la consulta como un array de objetos:
+		$groups = $results->fetchAll(PDO::FETCH_OBJ);
+
+		// Quitamos las variables que conectan a la base de datos:
+		unset($results);
+		unset($connection);
+
 	}
 }
-
-
-
-
-
-
 
 ?>
 
@@ -69,20 +101,35 @@ if (empty($_GET)) {
 <body>
 	<?php
 	require_once(
-		$_SERVER['DOCUMENT_ROOT'] . '\includes\headerGustavoVictor.inc.php');
+		$_SERVER['DOCUMENT_ROOT'] . 
+		'/DiscografiaGustavoVictor/includes/headerGustavoVictor.inc.php'
+	);
 	?>
 
 	<form action="#" method="get">
 		<label for="">Búsqueda</label>
 		<input
 			type="text"
-			name="busqueda"
-			id="busqueda"
+			name="search"
+			id="search"
 			placeholder="Busca tu grupo aquí">
 		<input type="submit" value="Buscar">
 	</form>
 
 	<h2>Grupos:</h2>
+	<?php
+		echo '<div class="groups>';
+		foreach ($groups as $group) {
+			echo '<div class="group">';
+			echo '<div class="gPhoto">';
+			echo '<img href"'. $group->img . '"/>';
+			echo '</div>';
+			echo '<h2>'. $group->name . '</h2>';
+			echo '</div>';
+		}
+		echo '</div>';
+
+	?>
 	<footer>
 		<small>Gustavo Víctor &copy; 2024</small>
 	</footer>
